c4a082fbf92e97a7d76f988e8de2afb46b7f5bbe
sehob2020 <70333786+sehob2020@users.noreply.github.com>
2021-08-31 15:52:38 +0200

    daily save about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,15 +1,26 @@
 import Styles from './about.module.css';
 
-
+import gents from '../../resources/images/distinguished-gents-ride.jpg'
 
 function About() {
     return(
         <div className={Styles.wrapper}>
             <header className={Styles.top}>
                 <div className={Styles.banner}>
-                    <h1 className={Styles.bannerh1}>We Were Born To Race</h1>
+                    <h1 className={Styles.h1}>We Were Born To Race</h1>
                 </div>
             </header>
+
+            <section className={Styles.middle}>
+                <div className={Styles.gentsDiv}>
+                    <img className={Styles.gentsImg} src={gents} alt="Gentlemen" />
+                </div>
+                    
+                <div className={Styles.middeText}>
+                    <h2 className={Styles.h2}>About Us</h2>
+                    <p className={Styles.p}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui vulputate eu lectus sapien morbi odio. Scelerisque quis duis facilisis nunc, in augue porttitor. Eleifend pharetra urna pellentesque sed at ultricies sem. Sed odio eu et ut mattis sagittis venenatis sed maecenas.</p>
+                </div>
+            </section>
 
 
 
