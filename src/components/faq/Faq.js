b93177365b23540757461c4494dd9e4ba156058a
SamuelMeijer <Samuel.J.Meijer@gmail.com>
2021-08-31 21:24:23 +0200

    Addin styling to FAQ-component

--- src/components/faq/Faq.js
+++ src/components/faq/Faq.js
@@ -1,47 +1,47 @@
 // Importing CSS
-
-import { useState } from 'react';
 import Styles from './faq.module.css';
 
+// Importing useState from react
+import { useState } from 'react';
 
-// DOLDA SVAR, KLICK -> VISAR DET MAN KLICKADE PÅ (ENBART DET, RESTEN DÖLJAS)
 
 export default function Faq () {
-    const [clickedQuestion, setClickedQuestion] = useState(undefined)
+    // Defining state for 'clickedQuestion'
+    const [clickedQuestion, setClickedQuestion] = useState(undefined);
 
     // TODO: Replace with import when translate is done
     const faqQuestions = [
         {
             id: 0,
-            question: 'Måste jag ha motorcykelkörkort för att köpa',
+            question: 'Måste jag ha motorcykelkörkort för att köpa?',
             answer: 'Answer 1: Japp!'
         },
         {
             id: 1,
-            question: 'Vad säljer ni för motorcyklar',
+            question: 'Vad säljer ni för motorcyklar?',
             answer: 'Answer 2: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta reprehenderit sunt eum quidem deserunt porro. '
         },
         {
             id: 2,
-            question: 'Hur lång tid tar det att få en motorcykel',
+            question: 'Hur lång tid tar det att få en motorcykel?',
             answer: 'Answer 3: Lorem ipsum dolor sit amet.  '
         },
         {
             id: 3,
-            question: 'Vad kostar en motorcykel',
+            question: 'Vad kostar en motorcykel?',
             answer: 'Answer 4: Lorem ipsum dolor sit, amet consectetur adipisicing elit. '
         },
         {
             id: 4,
-            question: 'Hur kan man kontakta er',
+            question: 'Hur kan man kontakta er?',
             answer: 'Answer 5: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt rerum consequuntur ratione eaque minus accusantium nisi velit. '
         },
         {
             id: 5,
-            question: 'Vart kan man hitta er',
+            question: 'Vart kan man hitta er?',
             answer: 'Answer 6 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt quasi repudiandae veniam sed tempore. '
         },
-    ]
+    ];
 
     return (
         <section id={ Styles.faqWrapper }>
@@ -52,9 +52,11 @@ export default function Faq () {
                 { faqQuestions.map( (question) => {
                     return (
                         <li key={ question.id } onClick={() => { setClickedQuestion(question.id) }} className={ Styles.menuItem }>
-                            <h2>{ question.question }</h2>
+                            {/* TODO: Add fontawesome-chevron to question */}
+                            <h2 className={ Styles.menuItemQuestion }>{ question.question }</h2>
+                            
                             {   question.id === clickedQuestion ?
-                                    <p>{ question.answer }</p>
+                                    <p className={ Styles.menuItemAnswer }>{ question.answer }</p>
                                 :
                                     console.log('No question selected')
                             }
@@ -64,5 +66,5 @@ export default function Faq () {
                 })}
             </ul>
         </section>
-    )
-}
+    );
+};
