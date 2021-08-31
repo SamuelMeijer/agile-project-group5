a381b48c071045a93550aed22b9480dcee649cf2
SamuelMeijer <Samuel.J.Meijer@gmail.com>
2021-08-31 20:44:55 +0200

    Adding basic funtionality of FAQ-component

--- src/App.js
+++ src/App.js
@@ -6,12 +6,19 @@ import {
   Route
 } from "react-router-dom";
 
+import Faq from './components/Faq/Faq';
+
 function App() {
   return (
+    <div>
+      
       <Switch>
         {/* <Route path='/' component={ Index } exact /> */}
         {/* <Route path='/about' component={ About } /> */}
       </Switch>
+
+      <Faq />
+    </div>
   );
 }
 
