900b5fc0e7dae0f4e860b1b9c06b7286fb7baea5
sehob2020 <70333786+sehob2020@users.noreply.github.com>
2021-08-31 12:32:38 +0200

    pull branch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import About from './components/About/About'; 
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <About />
     </div>
   );
 }
---
+++ src/components/About/About.js
@@ -0,0 +1,19 @@
+import Styles from './about.module.css'
+
+function About() {
+    return(
+        <div className={Styles.wrapper}>
+            <div className={Styles.banner}>
+                <img className={Styles.bannerImg} src="" alt="Motorcyckle banner" />
+                <div className={Styles.bannerBox}>
+                    <h1 className={Styles.bannerh1}>We Were Born To Race</h1>
+                </div>
+            </div>
+            
+
+
+        </div>
+    )
+}
+
+export default About;
