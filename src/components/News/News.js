c3cf924f1d9ba7db12609fed95184e1b4cd3f68a
StefanHbg <70539225+StefanHbg@users.noreply.github.com>
2021-09-07 11:57:49 +0200

    added news routing

---
+++ src/components/News/News.js
@@ -0,0 +1,15 @@
+import Styles from './news.module.css'
+
+import { useTranslation, Trans } from 'react-i18next';
+
+function News() {
+    const { t, i18n } = useTranslation();
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default News;
