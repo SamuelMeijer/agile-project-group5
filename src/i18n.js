db6aef33cc48262b1019f1f445bb1899f991ed93
SamuelMeijer <Samuel.J.Meijer@gmail.com>
2021-09-07 10:33:39 +0200

    Adding functionality for translation FAQ

--- src/i18n.js
+++ src/i18n.js
@@ -46,9 +46,38 @@ i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
                     desc: 'Motorcycles are our passion. Together we have more than 30 years experience from selling, servicing and riding motorcyckels. We offer the best motorcycles at the lowest prices and will always ne there for our customers.',
                     employees: 'Employees',
                 },
-                faq: {
-
-                },
+                faq: [
+                    {
+                        id: 0,
+                        question: 'Måste jag ha motorcykelkörkort för att köpa? - ENGLISH',
+                        answer: 'Answer 1: Japp!'
+                    },
+                    {
+                        id: 1,
+                        question: 'Vad säljer ni för motorcyklar?',
+                        answer: 'Answer 2: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta reprehenderit sunt eum quidem deserunt porro. '
+                    },
+                    {
+                        id: 2,
+                        question: 'Hur lång tid tar det att få en motorcykel?',
+                        answer: 'Answer 3: Lorem ipsum dolor sit amet.  '
+                    },
+                    {
+                        id: 3,
+                        question: 'Vad kostar en motorcykel?',
+                        answer: 'Answer 4: Lorem ipsum dolor sit, amet consectetur adipisicing elit. '
+                    },
+                    {
+                        id: 4,
+                        question: 'Hur kan man kontakta er?',
+                        answer: 'Answer 5: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt rerum consequuntur ratione eaque minus accusantium nisi velit. '
+                    },
+                    {
+                        id: 5,
+                        question: 'Vart kan man hitta er?',
+                        answer: 'Answer 6 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt quasi repudiandae veniam sed tempore. '
+                    },
+            ],
                 contact: {
                     contact: 'Contact',
                     name: 'Name:',
@@ -91,9 +120,38 @@ i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
                     desc: 'Motorcyklar är våran passion. Tillsammans har vi mer än 30 års erfarenhet från försäljning, service och körande av motorcyklar. Vi erbjuder de bästa motorcycklarna till de lägsta priserna och kommer alltid att finnas där för våra kunder.',
                     employees: 'Anställda'
                 },
-                faq: {
-
-                },
+                faq: [
+                    {
+                        id: 0,
+                        question: 'Måste jag ha motorcykelkörkort för att köpa?',
+                        answer: 'Answer 1: Japp!'
+                    },
+                    {
+                        id: 1,
+                        question: 'Vad säljer ni för motorcyklar?',
+                        answer: 'Answer 2: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta reprehenderit sunt eum quidem deserunt porro. '
+                    },
+                    {
+                        id: 2,
+                        question: 'Hur lång tid tar det att få en motorcykel?',
+                        answer: 'Answer 3: Lorem ipsum dolor sit amet.  '
+                    },
+                    {
+                        id: 3,
+                        question: 'Vad kostar en motorcykel?',
+                        answer: 'Answer 4: Lorem ipsum dolor sit, amet consectetur adipisicing elit. '
+                    },
+                    {
+                        id: 4,
+                        question: 'Hur kan man kontakta er?',
+                        answer: 'Answer 5: Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt rerum consequuntur ratione eaque minus accusantium nisi velit. '
+                    },
+                    {
+                        id: 5,
+                        question: 'Vart kan man hitta er?',
+                        answer: 'Answer 6 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt quasi repudiandae veniam sed tempore. '
+                    },
+            ],
                 contact: {
                     contact: 'Kontakt',
                     name: 'Namn:',
